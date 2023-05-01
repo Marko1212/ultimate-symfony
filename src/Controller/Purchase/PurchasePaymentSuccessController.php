cadacee16b81766139825e8c63fa2fea92447557
Marko_Askovic <marko.askovic@casasoft.com>
2023-05-01 18:34:21 +0200

    events

--- src/Controller/Purchase/PurchasePaymentSuccessController.php
+++ src/Controller/Purchase/PurchasePaymentSuccessController.php
@@ -4,11 +4,13 @@ namespace App\Controller\Purchase;
 
 use App\Entity\Purchase;
 use App\Cart\CartService;
+use App\Event\PurchaseSuccessEvent;
 use App\Repository\PurchaseRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\Routing\Annotation\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\EventDispatcher\EventDispatcherInterface;
 
 class PurchasePaymentSuccessController extends AbstractController
 {
@@ -16,7 +18,7 @@ class PurchasePaymentSuccessController extends AbstractController
      * @Route("/purchase/terminate/{id}", name="purchase_payment_success")
      * @IsGranted("ROLE_USER")
      */
-    public function success($id, PurchaseRepository $purchaseRepository, EntityManagerInterface $em, CartService $cartService)
+    public function success($id, PurchaseRepository $purchaseRepository, EntityManagerInterface $em, CartService $cartService, EventDispatcherInterface $dispatcher)
     {
         // 1. Je recupere la commande
         $purchase = $purchaseRepository->find($id);
@@ -33,6 +35,10 @@ class PurchasePaymentSuccessController extends AbstractController
 
         // 3. Je vide le panier
         $cartService->empty();
+
+        // Lancer un evenement qui permette aux autres developpeurs de reagir a la prise d'une commande
+        $purchaseEvent = new PurchaseSuccessEvent($purchase);
+        $dispatcher->dispatch($purchaseEvent, 'purchase.success');
 
         // 4. Je redirige avec un flash vers la liste des commandes
         $this->addFlash('success', "La commande a ete payee et confirmee !");
---
+++ src/EventDispatcher/PurchaseSuccessEmailSubscriber.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\EventDispatcher;
+
+use App\Event\PurchaseSuccessEvent;
+use Psr\Log\LoggerInterface;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+
+class PurchaseSuccessEmailSubscriber implements EventSubscriberInterface
+{
+    public function __construct(private readonly LoggerInterface $logger)
+    {
+    }
+
+    public static function getSubscribedEvents()
+    {
+        return [
+            'purchase.success' => 'sendSuccessEmail'
+        ];
+    }
+
+    public function sendSuccessEmail(PurchaseSuccessEvent $purchaseSuccessEvent)
+    {
+        $this->logger->info("Email envoye pour la commande n° " . $purchaseSuccessEvent->getPurchase()->getId());
+    }
+}
